dcf0b65a31a8106bf54358ed4ad97a264fdc788d
Ray Grant <raykyleg@amazon.com>
2023-03-11 15:16:50 -0700

    Initial commit

---
+++ test/cdkpip.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdkpip from '../lib/cdkpip-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdkpip-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdkpip.CdkpipStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
