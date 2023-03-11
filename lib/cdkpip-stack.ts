e70bd3ae6638723cc8ac54b6a3804780f319ed21
Ray Grant <raykyleg@amazon.com>
2023-03-11 15:38:15 -0700

    update github

--- lib/cdkpip-stack.ts
+++ lib/cdkpip-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline } from 'aws-cdk-lib/aws-events-targets';
-import { CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,12 +7,10 @@ export class CdkpipStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
-
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('......', 'main'),
+        input: CodePipelineSource.gitHub('ray836/grv-cdk-pipeline', 'main'),
         commands: ['npm ci',
                    'npm run build',
                    'npx cdk synth'
@@ -21,11 +18,5 @@ export class CdkpipStack extends cdk.Stack {
       }),
     });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkpipQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
